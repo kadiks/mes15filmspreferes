c26e9c9aa02178037cdcbc63c310540064029af0
Jénaïc Cambré <me@jenaiccambre.com>
2017-12-08 10:20:11 +0100

    change absolute result to %

--- src/sorter/moviesByCountry.js
+++ src/sorter/moviesByCountry.js
@@ -2,6 +2,10 @@ const _ = require("lodash");
 
 module.exports = movies => {
   const groups = {};
+  const total = {
+    movies: 0,
+    votes: 0
+  };
   movies.forEach(movie => {
     const { details, twitter } = movie;
     if (details.hasOwnProperty("production_countries") === false) {
@@ -21,13 +25,23 @@ module.exports = movies => {
       groups[countryCode].movies++;
       groups[countryCode].votes += count;
     }
+    total.movies++;
+    total.votes += count;
   });
   const keys = Object.keys(groups);
   const fmtGroups = [];
   keys.forEach(key => {
-    fmtGroups.push(groups[key]);
+    let { country, movies, votes } = groups[key]; // key is "countryCode here"
+    movies = parseInt(movies / total.movies * 10000, 10) / 100;
+    votes = parseInt(votes / total.votes * 10000, 10) / 100;
+    movies = `${movies} %`;
+    votes = `${votes} %`;
+    fmtGroups.push({ country, movies, votes });
   });
-  const sorted = _.sortBy(fmtGroups, [m => m.movies, m => m.votes]);
+  const sorted = _.sortBy(fmtGroups, [
+    m => parseInt(m.movies, 10),
+    m => parseInt(m.votes, 10)
+  ]);
   sorted.reverse();
   return sorted;
 };
